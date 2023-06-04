aacd7878a82884828ce3d99085da6b871f0843a7
Nicole Gottschall <ngottschall@web.de>
2023-06-04 12:03:37 +0200

    finalizes pr08-1 and pr08-2 for presentation

--- PR08/pr08-1.c
+++ PR08/pr08-1.c
@@ -10,33 +10,39 @@
 
 /*=====================================================================
  * 
- * Programm testet Umsetzung des Starts eines Kindprozesses
+ * Programm testet Umsetzung des Starts eines Kindprozesses wie folgt:
+ * 
+ * Programm welches einen Kindprozess erzeugt, seine eigene PID und die
+ * seines Elternteiles anzeigt, danach bis 1 Milliarde zaehlt und sich
+ * dann beendet.
+ * 
+ * Der Elternteil soll seine eigene und die PID des Kindes ausgeben und
+ * auf das Ende des Kindprozesses mit wait() warten.
+ * Sobald das Kind beendet ist, soll der Elternteil den Rueckgabewert
+ * des Kindes auswerten und ausgeben.
+ * Danach soll er sich beenden.
  * 
  * ===================================================================*/
-
-/* TODO: Prozesswissen kurz zusammenfassen */
 
 int main(int argc, char *argv[])
 {
 	int npid;
 	npid = fork(); /* ab hier zwei gleich laufende Programme */
 	
-	/* PID? Wie wird diese vergeben? */
-	/* Fehlerbehandlung?! */
-	
-	if (npid == -1) { 
-		/* Fehler ist aufgetreten */
+	if (npid == -1) { /* Elternprozess -1 im Fehlerfall */
+		/* Code im Fehlerfall */
 		perror("fork: ");
 		exit(EXIT_FAILURE);
 		
-	} else if (npid == 0) {
+	} else if (npid == 0) { /* Kindprozess immer 0 */
 		/* Code fuer Kindprozess */
-		/* Kindprozessbehandlung (Kindprozess immer 0) */
 		
 		/* Anzeige eigene PID und die seines Elternteiles */
-		printf("Ich bin ein Kinprozess.\n");
+		printf("Ich bin ein Kindprozess.\n");
 		printf("Meine PID: %d\n", getpid());
 		printf("Die PID meines Elternteiles (PPID): %d\n", getppid());
+		
+		printf("==================================================\n");
 		
 		/* Zaehlt bis 1 Milliarde und beendet sich dann */
 		int i;
@@ -46,38 +52,37 @@ int main(int argc, char *argv[])
 		}
 		
 		/* Systemruf zur Prozessbeendigung */
-		/* Wirkungsweise: schliesse offene Dateien, Elternprozess
-		 * erhaelt Signal SIGCHLD, falls Prozess Login-Shell ist,
-		 * erhaelt gesamte Prozessgruppe SIGHUP, noch existierende
-		 * Kinder werden vom Init-Prozess (PID 1) "adoptiert" */
-		 
-		 /* Unterschied exit() zu _exit() */
-		_exit(EXIT_SUCCESS); 
+		/* exit() stellt geordneten Prozessabschluss mit korrekter
+			Ressourcenfreigabe sicher */	
+		/* Sendet Signal SIGCHLD an Elternprozess - signalisiert Ende */	 
+		exit(EXIT_SUCCESS); 
 		
-	} else {
+	} else { /* Elternprozess PID des Kindes bei erfolgreichen fork() */
 		/* Code fuer Elternprozess */
-		/* Elternprozessbehandlung bei erfolgreichem fork() 
-		 * Elternprozess PID des Kindes oder -1 bei Fehler */
 		 
 		 /* Anzeige eigenes PID und die des Kindes */
 		 printf("Ich bin ein Elternprozess.\n");
 		 printf("Meine PID: %d\n", getpid());
 		 printf("Die PID meines Kindes: %d\n", npid);
 		 
-		 /* Wartet auf das Ende seines Kindprozess mit wait() */
-		 /* Umsetzung stellt sicher, dass auf alle Kindprozese 
-				gewartet wird - erst wenn alle zurueckgekehrt sind
-				wird zu naechster Anweisung uebergegangen */
-		 int status;
-		 /* Elternprozess wartet auf Ende des Kindprozesses */
-		 /* Ende erwarten, um deren Exit-Status/ Rueckgabewert
-		  * auswerten zu koennen */
-		 /* Funktionsweise: */
-		 while(wait(&status) > 0);
+		 printf("==================================================\n");
 		 
-		 /* Rueckgabewert des Kindes auswerten */
-		 printf("Rueckgabewert des Kindprozesses: %d\n", 
-			WEXITSTATUS(status));
+		 /* Wartet auf das Ende eines Kindprozess mit wait(), um 
+				Exit-Status Kindprozess auswerten zu koennen */
+		 /* Ermittelt dabei Status des ersten beendeten Kindprozesses */
+		 /* Schleife -> auf Beenden aller Kindprozesse warten 
+				Rueckgabe -1 -> alle Kindprozesse beendet */
+		 
+		 int wstatus;
+		 while(wait(&wstatus) > 0) {
+			/* Rueckgabewert des Kindes auswerten */
+			/* Wenn Kindprozess (normal) beendet wurde return true */
+			/* "normal" = exit(), _exit(), return from main() */
+			if(WIFEXITED(wstatus)) {
+				printf("Rueckgabewert des Kindprozesses: %d\n", 
+				WEXITSTATUS(wstatus));
+			}
+		 }
 	}
 		
 	return EXIT_SUCCESS;
--- PR08/pr08-2.c
+++ PR08/pr08-2.c
@@ -10,40 +10,41 @@
 
 /*=====================================================================
  * 
- * Programm testet Umsetzung des Starts eines Kindprozesses
+ * Programm testet Umsetzung des Starts eines Kindprozesses wie folgt:
+ * 
+ * Programm welches einen Kindprozess erzeugt, seine eigene PID und die
+ * seines Elternteiles anzeigt, danach bis 1 Milliarde zaehlt und sich
+ * dann beendet.
+ * 
+ * Der Elternteil soll seine eigene und die PID des Kindes ausgeben und
+ * in einer Schleife jeweils nach 1 Sekunde (sleep(1)) pruefen, ob
+ * Kind schon fertig ist.
+ * Zustand "läuft" oder "fertig" ist jedes mal auf Konsole auszugeben.
+ * Sobald das Kind beendet ist, soll der Elternteil den Rueckgabewert
+ * des Kindes auswerten und ausgeben.
+ * Danach soll er sich beenden.
  * 
  * ===================================================================*/
-
-/* TODO: Prozesswissen kurz zusammenfassen */
-/* Tipp: Recherchieren man page zu Funktionen wait() und waitpid() 
- * deren Parameter und die verfuegbaren Optionen. */
-/* wait wartet auf ersten beendeten Kindprozess */
-/* waitpid() gezielte Ueberwachung bestimmten Kindprozesses */
-/* zu jeder BS-Funktion gehoert Test*/
-/* printf()-Ausgabe erfolgt erst, wenn Puffer voll ist oder 
- * Zeilenumbruch erfolgt */
 
 int main(int argc, char *argv[])
 {
 	int npid;
 	npid = fork(); /* ab hier zwei gleich laufende Programme */
 	
-	/* PID? Wie wird diese vergeben? */
-	/* Fehlerbehandlung?! */
-	
-	if (npid == -1) { 
-		/* Fehler ist aufgetreten */
+	if (npid == -1) { /* Elternprozess -1 im Fehlerfall */
+		/* Code im Fehlerfall */
 		perror("fork: ");
 		exit(EXIT_FAILURE);
 		
-	} else if (npid == 0) {
+	} else if (npid == 0) { /* Kindprozess immer 0 */
 		/* Code fuer Kindprozess */
-		/* Kindprozessbehandlung (Kindprozess immer 0) */
 		
 		/* Anzeige eigene PID und die seines Elternteiles */
-		printf("Ich bin ein Kinprozess.\n");
+		printf("Ich bin ein Kindprozess.\n");
 		printf("Meine PID: %d\n", getpid());
 		printf("Die PID meines Elternteiles (PPID): %d\n", getppid());
+		
+		printf("==================================================\n");
 		
 		/* Zaehlt bis 1 Milliarde und beendet sich dann */
 		int i;
@@ -53,45 +54,55 @@ int main(int argc, char *argv[])
 		}
 		
 		/* Systemruf zur Prozessbeendigung */
-		/* Wirkungsweise: schliesse offene Dateien, Elternprozess
-		 * erhaelt Signal SIGCHLD, falls Prozess Login-Shell ist,
-		 * erhaelt gesamte Prozessgruppe SIGHUP, noch existierende
-		 * Kinder werden vom Init-Prozess (PID 1) "adoptiert" */
-		 
-		 /* TODO: Unterschied exit() zu _exit() */
-		_exit(EXIT_SUCCESS); 
+		/* exit() stellt geordneten Prozessabschluss mit korrekter
+			Ressourcenfreigabe sicher */	
+		/* Sendet Signal SIGCHLD an Elternprozess - signalisiert Ende */	 
+		exit(EXIT_SUCCESS); 
 		
-	} else {
+	} else { /* Elternprozess PID des Kindes bei erfolgreichen fork() */
 		/* Code fuer Elternprozess */
-		/* Elternprozessbehandlung bei erfolgreichem fork() 
-		 * Elternprozess PID des Kindes oder -1 bei Fehler */
 		 
 		 /* Anzeige eigenes PID und die des Kindes */
 		 printf("Ich bin ein Elternprozess.\n");
 		 printf("Meine PID: %d\n", getpid());
 		 printf("Die PID meines Kindes: %d\n", npid);
 		 
-		 /* Wartet aktiv auf Ende des Kindprozesses */
-		 /* Prueft in Schleife jeweils nach 1 Sekunde, ob
-		  * Kind schon fertig ist */
-		 int status;
-		 while (1) {
-			int result = waitpid(npid, &status, WNOHANG);
-			if(result == -1) {
-				/* Fehler beim waitpid() */
-				/* Welche moegliche Fehler? */
-				perror("waitpid: \n");
+		 printf("==================================================\n");
+		 
+		 /* Jede Sekunde pruefen, ob Kind schon fertig ist */
+		 
+		 /* wait() wartet auf ersten beendeten Kindprozess */
+		 /* waitpid() gezielte Ueberwachung bestimmten Kindprozesses */
+		 
+		 int wstatus;
+		 while(1) {
+			 
+			 sleep(1); /* aller einer Sekunde ueberpruefen */
+			 
+			 /* WNOHANG - mit Option unterbricht waitpid() Ausfuehrung
+					nicht, sollte Kind nicht fertig gewurden sein */
+			 /* npid ist PID Kindprozess bei erfolgreichem fork() */
+			int exitStatus = waitpid(npid, &wstatus, WNOHANG);
+			
+			if(exitStatus == -1) { /* Fehler waitpid() */
+				perror("waitpid: ");
 				exit(EXIT_FAILURE);
-			} else if (result == 0) {
-				/*Kindprozess noch nicht beendet */
+				
+			} else if(exitStatus == 0) { /* Kindprozess nicht fertig */
 				printf("läuft\n");
-				sleep(1);
-			} else {
-				/* Kindprozess ist beendet */
+				
+			} else { /* Kindprozess fertig/beendet */
+				printf("===========================================\n");
 				printf("fertig\n");
+				
 				/* Rueckgabewert des Kindes auswerten */
-				 printf("Rueckgabewert des Kindprozesses: %d\n", 
-					WEXITSTATUS(status));
+				/* Wenn Kindprozess (normal) beendet wurde -> true */
+				/* "normal" = exit(), _exit(), return from main() */
+				if(WIFEXITED(wstatus)) {
+					printf("Rueckgabewert des Kindprozesses: %d\n", 
+					WEXITSTATUS(wstatus));
+				}
+				
 				/* Schleife verlassen */
 				break;
 			}
